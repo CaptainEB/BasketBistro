c0b8cb10ecd401d16a399622d0a2f4884ec2ac33
Sam <Sambose426@yahoo.com>
2023-12-04 22:17:10 -0700

    added basic front end structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    createHttpLink,
-  } from '@apollo/client';
-  import { setContext } from '@apollo/client/link/context';
-  import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import './App.scss';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import PageRoutes from './components/Router';
+
+export default function App() {
+	return (
+		<>
+			<header>
+				<Nav />
+			</header>
+			<main>
+				<PageRoutes />
+			</main>
+			<Footer />
+		</>
+	);
+}
---
+++ client/src/components/Router/PageRouter.jsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from 'react-router-dom';
+
+export default function PageRoutes() {
+	return (
+		<Routes>
+			{/* routes go here */}
+			{/* <Route path="/profile" element={<Test />} /> */}
+		</Routes>
+	);
+}
