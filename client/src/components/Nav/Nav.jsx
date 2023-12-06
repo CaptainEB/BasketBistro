947aa858d52648f668574c5f7ea0ae9a5bb29d4d
Sam <Sambose426@yahoo.com>
2023-12-05 20:38:42 -0700

    basic styling

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,3 +1,34 @@
+import { Link } from 'react-router-dom';
+import './Nav.scss';
+
 export default function Nav() {
-	return <div>Nav</div>;
+	return (
+		<nav>
+			<ul className="nav-ul">
+				<h1>Basket Bistro</h1>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+
+				<li>
+					<Link to="/login">Login</Link>
+				</li>
+
+				<li>
+					<Link to="/signup">Signup</Link>
+				</li>
+
+				<li>
+					<Link to="/myrecipes">My Recipes</Link>
+				</li>
+
+				<li>
+					<Link to="/currentlist">Current List</Link>
+				</li>
+				<li>
+					<Link to="/profile">Profile</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
