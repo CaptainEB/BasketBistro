ce77ab13e280efce0113e356e6de31eb02e86b1c
tdavids215 <105099239+tdavids215@users.noreply.github.com>
2023-12-12 21:31:33 -0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/basket-bistro');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/basketBistro');
 
 module.exports = mongoose.connection;
