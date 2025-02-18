a2b7eb667ec517f5cfacfe7899280b77c22bcbf7
sam eb <sambose426@yahoo.com>
2025-02-17 22:47:57 -0600

    updated connectionJS file to be compatible with railway

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/basketBistro');
+mongoose.connect(
+	process.env.MONGO_URL,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	} || 'mongodb://127.0.0.1:27017/basketBistro'
+);
 
 module.exports = mongoose.connection;
